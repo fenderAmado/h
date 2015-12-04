ec45d893ece2eb005e8ce1eeec078652ab75a11f
fenderAmado <cdavfender@hotmail.com>
2015-12-04 17:14:20 -0500

    subida

---
+++ index.php
@@ -0,0 +1,28 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>oficial embroidery</title>
+	<style type="text/css">
+
+
+	</style>
+</head>
+<body style="background-image : url(imagen/fondo.jpg)">
+
+<center>
+	
+	 <font face="agency FB,Bradley Hand ITC" style="font-size: 30px">Bienvenidos<br/></font>
+	 <font face="agency FB,Bradley Hand ITC" style="font-size: 30px">a<br/></font>
+	 <font face="agency FB,Bradley Hand ITC" style="font-size: 30px">Embroidery Store<br/></font>
+
+	 	<img src="imagen/inicio.PNG">
+
+
+
+</center>
+
+
+</body>
+</html>
